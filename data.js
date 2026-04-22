b909a9ed54a2db71e72d5d7ee581c5e392e4fcd5
exiwestdeni-star <exiwestdeni@gmail.com>
2026-04-22 16:56:35 +0300

    Create data.js

---
+++ data.js
@@ -0,0 +1,33 @@
+const products = [
+
+/* ================= iPhone ================= */
+{cat:"iphone",name:"iPhone 17 256GB Black",price:59900,img:"https://i.imgur.com/1.png"},
+{cat:"iphone",name:"iPhone 17 256GB Blue",price:59200,img:"https://i.imgur.com/1.png"},
+{cat:"iphone",name:"iPhone 17 256GB White",price:61400,img:"https://i.imgur.com/1.png"},
+
+{cat:"iphone",name:"iPhone 17 Pro 256GB Blue",price:94300,img:"https://i.imgur.com/2.png"},
+{cat:"iphone",name:"iPhone 17 Pro 512GB White",price:113000,img:"https://i.imgur.com/2.png"},
+
+{cat:"iphone",name:"iPhone 17 Pro Max 256GB Blue",price:103000,img:"https://i.imgur.com/3.png"},
+{cat:"iphone",name:"iPhone 17 Pro Max 1TB Orange",price:124900,img:"https://i.imgur.com/3.png"},
+
+/* ================= Watch ================= */
+{cat:"watch",name:"Apple Watch S11 46mm",price:25900,img:"https://i.imgur.com/watch.png"},
+{cat:"watch",name:"Apple Watch Ultra 3",price:59300,img:"https://i.imgur.com/watch.png"},
+
+/* ================= iPad ================= */
+{cat:"ipad",name:"iPad Air M4 256GB",price:57000,img:"https://i.imgur.com/ipad.png"},
+{cat:"ipad",name:"iPad Pro 13 M5",price:96000,img:"https://i.imgur.com/ipad.png"},
+
+/* ================= Mac ================= */
+{cat:"mac",name:"MacBook Air M4 512GB",price:91000,img:"https://i.imgur.com/mac.png"},
+
+/* ================= PS ================= */
+{cat:"ps",name:"PS5 Pro 2TB",price:74500,img:"https://i.imgur.com/ps.png"},
+
+/* ================= Access ================= */
+{cat:"access",name:"AirPods Pro 2",price:14000,img:"https://i.imgur.com/airpods.png"},
+{cat:"access",name:"AirPods Max",price:40000,img:"https://i.imgur.com/airpods.png"},
+{cat:"access",name:"Magic Mouse",price:9000,img:"https://i.imgur.com/mouse.png"}
+
+];
