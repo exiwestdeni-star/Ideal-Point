c608f0a1471fc39411c37e57287e7f9a9e2f4dc3
exiwestdeni-star <exiwestdeni@gmail.com>
2026-04-22 17:00:28 +0300

    Update data.js

--- data.js
+++ data.js
@@ -1,33 +1,127 @@
 const products = [
 
-/* ================= iPhone ================= */
-{cat:"iphone",name:"iPhone 17 256GB Black",price:59900,img:"https://i.imgur.com/1.png"},
-{cat:"iphone",name:"iPhone 17 256GB Blue",price:59200,img:"https://i.imgur.com/1.png"},
-{cat:"iphone",name:"iPhone 17 256GB White",price:61400,img:"https://i.imgur.com/1.png"},
+/* ================= IPHONE ================= */
 
-{cat:"iphone",name:"iPhone 17 Pro 256GB Blue",price:94300,img:"https://i.imgur.com/2.png"},
-{cat:"iphone",name:"iPhone 17 Pro 512GB White",price:113000,img:"https://i.imgur.com/2.png"},
+{
+cat:"iphone",
+name:"iPhone 17",
+price:59000,
+img:"https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/iphone-15-blue-select-202309"
+},
 
-{cat:"iphone",name:"iPhone 17 Pro Max 256GB Blue",price:103000,img:"https://i.imgur.com/3.png"},
-{cat:"iphone",name:"iPhone 17 Pro Max 1TB Orange",price:124900,img:"https://i.imgur.com/3.png"},
+{
+cat:"iphone",
+name:"iPhone 17 Pro",
+price:94300,
+img:"https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/iphone-15-pro-natural-select"
+},
 
-/* ================= Watch ================= */
-{cat:"watch",name:"Apple Watch S11 46mm",price:25900,img:"https://i.imgur.com/watch.png"},
-{cat:"watch",name:"Apple Watch Ultra 3",price:59300,img:"https://i.imgur.com/watch.png"},
+{
+cat:"iphone",
+name:"iPhone 17 Pro Max",
+price:102500,
+img:"https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/iphone-15-pro-max-blue-select"
+},
 
-/* ================= iPad ================= */
-{cat:"ipad",name:"iPad Air M4 256GB",price:57000,img:"https://i.imgur.com/ipad.png"},
-{cat:"ipad",name:"iPad Pro 13 M5",price:96000,img:"https://i.imgur.com/ipad.png"},
+{
+cat:"iphone",
+name:"iPhone 17 Air",
+price:67000,
+img:"https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/iphone-14-blue-select"
+},
 
-/* ================= Mac ================= */
-{cat:"mac",name:"MacBook Air M4 512GB",price:91000,img:"https://i.imgur.com/mac.png"},
+/* ================= WATCH ================= */
+
+{
+cat:"watch",
+name:"Apple Watch S11",
+price:25900,
+img:"https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/watch-s9-midnight-select"
+},
+
+{
+cat:"watch",
+name:"Apple Watch Ultra",
+price:59300,
+img:"https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/watch-ultra-2-select"
+},
+
+/* ================= IPAD ================= */
+
+{
+cat:"ipad",
+name:"iPad 11",
+price:28800,
+img:"https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/ipad-10th-gen-blue-select"
+},
+
+{
+cat:"ipad",
+name:"iPad Air",
+price:47000,
+img:"https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/ipad-air-select"
+},
+
+{
+cat:"ipad",
+name:"iPad Pro",
+price:96000,
+img:"https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/ipad-pro-13-select"
+},
+
+/* ================= MAC ================= */
+
+{
+cat:"mac",
+name:"MacBook Air",
+price:91000,
+img:"https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/macbook-air-13-midnight-select"
+},
+
+{
+cat:"mac",
+name:"MacBook Pro",
+price:139000,
+img:"https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/macbook-pro-14-spaceblack-select"
+},
 
 /* ================= PS ================= */
-{cat:"ps",name:"PS5 Pro 2TB",price:74500,img:"https://i.imgur.com/ps.png"},
 
-/* ================= Access ================= */
-{cat:"access",name:"AirPods Pro 2",price:14000,img:"https://i.imgur.com/airpods.png"},
-{cat:"access",name:"AirPods Max",price:40000,img:"https://i.imgur.com/airpods.png"},
-{cat:"access",name:"Magic Mouse",price:9000,img:"https://i.imgur.com/mouse.png"}
+{
+cat:"ps",
+name:"PS5 Pro",
+price:74500,
+img:"https://images.unsplash.com/photo-1606813907291-d86efa9b94db"
+},
+
+/* ================= ACCESS ================= */
+
+{
+cat:"access",
+name:"AirPods 4",
+price:8800,
+img:"https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/airpods-3rd-gen-select"
+},
+
+{
+cat:"access",
+name:"AirPods Pro 2",
+price:14000,
+img:"https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/airpods-pro-2nd-gen-select"
+},
+
+{
+cat:"access",
+name:"AirPods Max",
+price:40000,
+img:"https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/airpods-max-select"
+},
+
+{
+cat:"access",
+name:"Magic Mouse",
+price:8000,
+img:"https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/magic-mouse-2024-black-select"
+}
 
 ];
