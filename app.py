826e2f15ac3cd26f28aec3a892646b23f8071cf4
exiwestdeni-star <exiwestdeni@gmail.com>
2026-04-22 14:38:20 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,22 +1,91 @@
 from flask import Flask, jsonify
 import os
+import random
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return "API работает"
+def price(base):
+    return base - random.randint(2000,3000)
 
 @app.route("/products")
 def products():
-    return jsonify([
-        {"name": "iPhone 16 256GB", "price": 99900},
-        {"name": "iPhone 16 Pro", "price": 119900},
-        {"name": "iPhone 17 Pro Max", "price": 149900},
-        {"name": "AirPods Pro 2", "price": 20900},
-        {"name": "MacBook Air M3", "price": 119900},
-        {"name": "PlayStation 5", "price": 64900}
-    ])
+    data = []
+
+    # iPhone 16 / 17
+    iphones = [
+        ("iPhone 16 256GB", 109900),
+        ("iPhone 16 Pro 256GB", 129900),
+        ("iPhone 16 Pro Max 256GB", 149900),
+        ("iPhone 17 256GB", 119900),
+        ("iPhone 17 Pro 256GB", 139900),
+        ("iPhone 17 Pro Max 256GB", 159900),
+    ]
+
+    for name, p in iphones:
+        data.append({
+            "name": name,
+            "price": price(p),
+            "image": "https://via.placeholder.com/200",
+            "category": "iphone"
+        })
+
+    # MacBook
+    macs = [
+        ("MacBook Air M3", 119900),
+        ("MacBook Pro M3", 159900),
+    ]
+
+    for name, p in macs:
+        data.append({
+            "name": name,
+            "price": price(p),
+            "image": "https://via.placeholder.com/200",
+            "category": "mac"
+        })
+
+    # iPad
+    ipads = [
+        ("iPad 10", 59900),
+        ("iPad Pro M2", 109900),
+    ]
+
+    for name, p in ipads:
+        data.append({
+            "name": name,
+            "price": price(p),
+            "image": "https://via.placeholder.com/200",
+            "category": "ipad"
+        })
+
+    # AirPods
+    airpods = [
+        ("AirPods 2", 13900),
+        ("AirPods Pro 2", 20900),
+    ]
+
+    for name, p in airpods:
+        data.append({
+            "name": name,
+            "price": price(p),
+            "image": "https://via.placeholder.com/200",
+            "category": "airpods"
+        })
+
+    # PlayStation
+    ps = [
+        ("PlayStation 5", 64900),
+        ("DualSense Controller", 7900),
+    ]
+
+    for name, p in ps:
+        data.append({
+            "name": name,
+            "price": price(p),
+            "image": "https://via.placeholder.com/200",
+            "category": "ps"
+        })
+
+    return jsonify(data)
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))
