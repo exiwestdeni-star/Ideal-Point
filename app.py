c3e63a257c10bd971104791097cb5e5f752159df
exiwestdeni-star <exiwestdeni@gmail.com>
2026-04-22 14:20:34 +0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "API работает"
+
+@app.route("/products")
+def products():
+    return jsonify([
+        {"name": "iPhone 16 256GB", "price": 99900},
+        {"name": "iPhone 16 Pro", "price": 119900},
+        {"name": "iPhone 17 Pro Max", "price": 149900},
+        {"name": "AirPods Pro 2", "price": 20900},
+        {"name": "MacBook Air M3", "price": 119900},
+        {"name": "PlayStation 5", "price": 64900}
+    ])
